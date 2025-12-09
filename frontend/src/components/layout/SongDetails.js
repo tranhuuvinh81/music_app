61a36d8cce31b22c56663f152b18ab358c4a4dd8
tranhuuvinh81 <vinht8802@gmail.com>
2025-12-09 22:35:37 +0700

    xong demo

--- frontend/src/components/layout/SongDetails.js
+++ frontend/src/components/layout/SongDetails.js
@@ -234,6 +234,13 @@ function SongDetails({ onExpand, openAddModal, openArtistModal }) {
                 fill="currentColor"
                 viewBox="0 0 20 20"
               >
+                <button
+                onClick={onExpand}
+                className="absolute top-2 right-2 p-2 bg-black/50 text-white rounded-full opacity-0 group-hover:opacity-100 transition-opacity z-20 hover:bg-black/70"
+                title="Toàn màn hình"
+              >
+                <FiMaximize2 size={16} />
+              </button>
                 <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
               </svg>
             </div>
